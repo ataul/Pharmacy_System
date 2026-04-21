7b7d0608178c089d5305adeeac9e8796eede9a74
Ataul Haque <ataul.haque@gmail.com>
2026-04-21 11:21:47 +0600

    database migrate fix

--- database/migrations/2023_04_05_104314_setup_countries_table.php
+++ database/migrations/2023_04_05_104314_setup_countries_table.php
@@ -14,7 +14,7 @@ class SetupCountriesTable extends Migration {
 		// Creates the users table
 		Schema::create(\Config::get('countries.table_name'), function($table)
 		{
-		    $table->integer('id')->unsigned()->index();
+		    $table->unsignedBigInteger('id')->primary();
 		    $table->string('capital', 255)->nullable();
 		    $table->string('citizenship', 255)->nullable();
 		    $table->string('country_code', 3)->default('');
@@ -33,7 +33,7 @@ class SetupCountriesTable extends Migration {
 		    $table->string('calling_code', 3)->nullable();
 		    $table->string('flag', 6)->nullable();
 
-		    $table->primary('id');
+		    //$table->primary('id');
 		});
 	}
 
